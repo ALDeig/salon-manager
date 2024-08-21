d9bb707ce599c50941e979ee6b2fa67b4192c17a
aldeig <alkin.denis@gmail.com>
2024-08-21 18:31:34 +0400

    feat: check user in start

--- app/src/dialogs/handlers/user.py
+++ app/src/dialogs/handlers/user.py
@@ -4,11 +4,17 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from app.src.dialogs.keyboards.user import kb_user_menu
+from app.src.services.db.dao.holder import HolderDao
+from app.src.services.texts import shift_texts
+from app.src.services.user import check_user
 
 router = Router()
 
 
-@router.message(Command("start"))
-async def cmd_start(msg: Message, state: FSMContext):
+@router.message(Command("start"), flags={"dao": True})
+async def cmd_start(msg: Message, state: FSMContext, dao: HolderDao):
+    if not await check_user(dao, msg.chat.username):
+        await msg.answer(shift_texts.USER_NOT_FOUND)
+        return
     await msg.answer("Меню", reply_markup=kb_user_menu())
     await state.clear()
